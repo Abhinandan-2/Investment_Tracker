0ff98e5f17100349f443d8b8a8de288ff863faf3
Abhinandan-2 <abhinandan24072000@gmail.com>
2024-02-29 14:07:22 +0530

    remove extra api

--- investment_tracker_frontend/src/APIs/Investment-api.js
+++ investment_tracker_frontend/src/APIs/Investment-api.js
@@ -1,3 +1,3 @@
 const investment_url="http://localhost:8085/api/users";
-const investment_ur="http://localhost:8000/api/users";
+// const investment_ur="http://localhost:8000/api/users";
 export default investment_url;
--- investment_tracker_frontend/src/APIs/Transaction-api.js
+++ investment_tracker_frontend/src/APIs/Transaction-api.js
@@ -1,3 +1,3 @@
 const transaction_api = "http://localhost:8082/api/users/transaction-api";
-const transaction_ap = "http://localhost:8000/api/users/transaction-api";
+// const transaction_ap = "http://localhost:8000/api/users/transaction-api";
 export default transaction_api;
